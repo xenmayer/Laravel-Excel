5e73abb2aaf067ffd48fc89b74668ffb6522235e
Nikita Vasilchenko <xenmayer@gmail.com>
2018-01-23 13:33:30 +0300

    Add flattener for sheet values

--- src/Maatwebsite/Excel/Traits/LaravelExcelWorksheetTemplaterTrait.php
+++ src/Maatwebsite/Excel/Traits/LaravelExcelWorksheetTemplaterTrait.php
@@ -5,6 +5,23 @@ trait LaravelExcelWorksheetTemplaterTrait
 {
     public function templatize ( array $placeholdersMap )
     {
-        dd( $this );
+        return $this->flattenSheetValues();
+    }
+
+    public function flattenSheetValues ()
+    {
+        $flattenedSheetValues = [];
+
+        foreach ( $this->toArray() as $rowCount => &$row ) {
+            foreach ( $row as $cellCount => &$cell ) {
+                $flattenedSheetValues[] = [
+                    'value'   => $cell,
+                    'cellKey' => $cellCount,
+                    'rowKey'  => $rowCount + 1,
+                ];
+            }
+        }
+
+        return $flattenedSheetValues;
     }
 }
